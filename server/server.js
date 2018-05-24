389819eb414926ffe658b6381a0dd99a31aa0c12
Mihai Turcu <mturcu@gmail.com>
2018-05-24 11:10:11 +0300

    Update

--- server/server.js
+++ server/server.js
@@ -28,6 +28,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+  .then(todos => {
+    res.send({todos});
+  })
+  .catch(e => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
